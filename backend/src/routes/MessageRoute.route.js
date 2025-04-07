6bf687b573fc145f0dbe12b520e7dc36b6c3eba2
mrnarendra9867 <narendrakumark750@gmail.com>
2025-04-07 18:46:59 +0530

    fixed files

--- backend/src/routes/MessageRoute.route.js
+++ backend/src/routes/MessageRoute.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { getUsersForSidebar, getMessage, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
