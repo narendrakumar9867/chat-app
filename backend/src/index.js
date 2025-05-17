021ec0ef6f05d7936eb12af31187a9b27a1ba36f
mrnarendra9867 <narendrakumark750@gmail.com>
2025-05-17 19:31:17 +0530

    complete deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 import authRoute from "./routes/auth.route.js";
 import MessageRoute from "./routes/MessageRoute.route.js";
@@ -11,6 +13,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Increase payload size limit
 app.use(express.json({ limit: '10mb' }));
@@ -26,6 +29,14 @@ app.use(cors(
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", MessageRoute);
+
+if(process.env.NODE_ENV==="production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`server is started at ${PORT}`);
