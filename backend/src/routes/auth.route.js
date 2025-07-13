552ce95f23c931711b759d540fbcad088107ccb0
mrnarendra9867 <narendrakumark750@gmail.com>
2025-07-14 00:42:18 +0530

    google auth update

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,50 @@
 import express from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
 import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
-
 const router = express.Router();
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:7777/api/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+        }
+    )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+// ------------------ Custom Routes -------------------
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/google-login", login);
-
 router.put("/update-profile", protectRoute, updateProfile);
-
 router.get("/check", protectRoute, checkAuth);
+
+// ------------------ Google Auth Routes ------------------
+
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+router.get("/google/callback", passport.authenticate("google", {
+    failureRedirect: "http://localhost:5173/login",
+}), (req, res) => {
+    res.redirect("http://localhost:5173");
+});
+
+router.get("/logout", (req, res) => {
+    req.logout(() => {
+        res.redirect("http://localhost:5173/login");
+    });
+});
 
 export default router;
